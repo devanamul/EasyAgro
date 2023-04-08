b66097ca4cb3cb3481f0824d5bdd385fe532e084
Anamul <ahasan192110@bscse.uiu.ac.bd>
2023-04-08 21:32:51 +0600

    model created for orject track

--- EasyAgro/EasyAgro/models.py
+++ EasyAgro/EasyAgro/models.py
@@ -4,3 +4,12 @@ from django.contrib.auth.models import User
 
 class crops(models.Model):
 	name = models.CharField(max_length=150)
+
+class project(models.Model):
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	crop = models.ForeignKey(crops, on_delete=models.CASCADE)
+	area = models.FloatField()
+	currentN = models.FloatField()
+	currentP = models.FloatField()
+	currentK =  models.FloatField()
+	status = models.CharField(max_length=25)
